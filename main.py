a07ed72be5d3dcd92c932b171d18ffa4549071c9
Amartya pandey <amartyapandey19@gmail.com>
2021-06-30 09:09:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import cv2
+
+
+image = cv2.imread("Profile.jpg")
+grey_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+invert = cv2.GaussianBlur(invert,(21,21),0)
+invertedblur = cv2.divide(grey_image,invertedblur,scale=258.0)
+
+
+cv2.imwrite("Profile-sketch.png",sketch)
+
+
+
+
+
+
+
+
+
+
+
+
+
